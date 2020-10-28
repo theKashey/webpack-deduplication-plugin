e3a721ef601077f6f9e5890bbd0b3ded241867e5
Anton Korzunov <thekashey@gmail.com>
2020-10-28 12:59:36 +1100

    fix: presort cache result

--- src/utils.js
+++ src/utils.js
@@ -8,8 +8,10 @@ const isEqual = require('lodash/isEqual');
 const pickBy = require('lodash/pickBy');
 const transform = require('lodash/transform');
 const mkdirp = require('mkdirp');
+const alphaSort = require('alphanum-sort');
 
 const patchesPath = 'patches';
+
 
 const extractPackageName = (name, scope) => {
     const patchName = scope ? `${scope}+${name}` : name;
@@ -70,6 +72,13 @@ const getCacheKey = ({ patchedPackages, rootPath }) => {
 const filterOnlyDuplicates = (pkg) =>
     pkg.split(path.sep).filter((p) => p === 'node_modules').length > 1;
 
+const resortNames = (duplicates) => (
+  alphaSort(Object.keys(duplicates)).reduce((acc, item) => {
+      acc[item]=duplicates[item].sort((a, b) => a.length - b.length);
+      return acc;
+  }, {})
+)
+
 const CACHE_BUST = 1;
 
 const getDuplicatedPackages = (options = {}) => {
@@ -116,14 +125,14 @@ const getDuplicatedPackages = (options = {}) => {
         return value.length > 1 && !patchedPackages.includes(key);
     });
 
-    const cleanFromFalsePositives = transform(
+    const cleanFromFalsePositives = resortNames(transform(
         onlyDuplicates,
         (result, value, key) => {
             // eslint-disable-next-line no-param-reassign
             result[key] = extractProperDuplicates(value).sort();
         },
         {}
-    );
+    ));
 
     if (cacheFileName) {
         fs.writeFileSync(cacheFileName, JSON.stringify(cleanFromFalsePositives, null, 2), 'utf8');
